741bebbb84b486d7e9877380afdcbeefbda53587
Shawn Cooper <shawn.cooper@explorys.com>
2014-02-19 23:40:28 -0500

    ititial commit

---
+++ js/angular_application.js
@@ -0,0 +1,15 @@
+
+
+function exampleController($scope){
+  //$scope.example =  test_var;
+
+  
+  /* IF NEED TO UPDATE VARS */
+  // $scope.$apply(function(){
+  //$scope.example =  test_var;
+  // });
+
+
+  });
+
+
